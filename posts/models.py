8858501ec5397899f6748ae93231c2d01fc145f9
Vlad Teteruk <vladteteruk01@gmail.com>
2023-11-04 11:54:47 +0200

    Implement models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from __future__ import annotations
 
-# Create your models here.
+import os.path
+import uuid
+
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.text import slugify
+
+
+def create_unique_file_name(instance: Post, filename: str) -> str:
+    _, extension = os.path.splitext(filename)
+
+    #  MARK: slugify returns refactored text / uuid creates a unique id
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}.{extension}"
+
+    return os.path.join("uploads/posts_pictures/", filename)
+
+
+class Post(models.Model):
+    date_posted = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(null=True, upload_to=create_unique_file_name)
+    description = models.TextField()
+
+
+class Like(models.Model):
+    user = models.ForeignKey(get_user_model(), related_name="likes", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="likes", on_delete=models.CASCADE)
+    date_liked = models.DateTimeField(auto_now_add=True)
